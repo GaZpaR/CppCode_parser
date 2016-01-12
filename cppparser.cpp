79c8c66a8bd2e53b503242ac71a0ed7ebae0f4b4
GaZpAr <gazpar@localhost.localdomain>
2016-01-12 14:54:12 +0600

    [+] Added file openening and reading with iostream

--- cppparser.cpp
+++ cppparser.cpp
@@ -1,7 +1,32 @@
 #include <cppparser.h>
 
+using namespace std;
+
 int main(int argc, char** argv)
 {
-	printf("Hello mazafacka!\r\n");
-	return 0;
+  streampos size;
+  char * memblock;
+
+  ifstream file (argv[1], ios::in|ios::binary|ios::ate);
+
+	cout << "File's name is: "<< argv[1] << "\r\n";
+
+  if (file.is_open())
+  {
+    size = file.tellg();
+    memblock = new char [size];
+    file.seekg (0, ios::beg);
+    file.read (memblock, size);
+    file.close();
+
+    cout << "the entire file content is in memory\r\n";
+
+		for(uint32_t i=0; i < size; i++)cout << memblock[i];
+
+		cout << "\r\n";
+
+    delete[] memblock;
+  }
+  else cout << "Unable to open file\r\n";
+  return 0;
 }

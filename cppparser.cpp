e97152bf0d8077fedabe7bd0dffec4ee3ac5d987
Gazpar Vigo <gazpar@localhost.localdomain>
2016-02-05 08:45:04 +0600

    [+] Added new features
    Writing content of current directory and findning #define and #include directives

--- cppparser.cpp
+++ cppparser.cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-size_t calcNameLength(char *file)
+void printfilename(char *file)
 {
   size_t len = 0;
-  cout << "File is:" << file << endl;
+  
   while(1){
     if(*file == '\"' || *file == '<') break;
     file++;
@@ -19,9 +19,11 @@ size_t calcNameLength(char *file)
      	if(file[len] == '\"' || file[len] == '>') break;
 	len++;
     }
-  
-  
-  return len;
+    char *temp = new char[len+1];
+    snprintf(temp, len+1,"%s",file);
+    
+    printf("Included file name is: %s\r\n",temp);
+
 }
 
 int main(int argc, char** argv)
@@ -53,57 +55,65 @@ int main(int argc, char** argv)
     arbitarytree sometree(argv[1], (uint8_t*)memblock, size);
 
     cout << "The entire file content is in memory" << endl;
-		cout << "File size is:" << size << " bytes" << endl;
-		cout << endl;
-		cout << "<File Content>" << endl;
-		cout << endl;
-
-		cout << memblock;
-
-		cout << endl;
-		cout << "</File Content>" << endl;
+    cout << "File size is:" << size << " bytes" << endl;
+    cout << endl;
+    cout << "<File Content>" << endl;
+    cout << endl;
+    //cout << memblock;
+    cout << endl;
+    cout << "</File Content>" << endl;
 
     //Here should be dependency resolver
+  char *current_catalog = new char[1024*2];
+  size_t cc_pathlen;
+  current_catalog = getcwd(current_catalog, cc_pathlen);
+  cout << "Path to curent catalog is:" << endl << current_catalog << endl;
+  cout << "Path length is: " << cc_pathlen << endl;
+  DIR *dir;
+  dir = opendir(current_catalog);
+  struct dirent *entry;
+  uint32_t num = 0;
+  while((entry = readdir(dir)) != NULL){
+    cout << "File " << num << " is:\"" << entry->d_name << "\"" << endl;
+    num++;
+  }
+
+  closedir(dir);
+
+  ifstream dotfile("..", ios::in | ios::binary | ios::ate);
+  if(dotfile.is_open()){
+    
+    cout << "Dot file is  opened" << endl;
+    int dsize = dotfile.tellg();
+    
+    cout << "Dot file length is: " << dsize << endl;
+
+    if(dsize > 0){
+      char *dotf = new char[dsize];
+      dotfile.seekg (0, ios::beg);
+      dotfile.read (dotf, dsize);
+      dotfile.close();
+
+      cout << "Dot file content is:"<< endl << dotf << endl;
+
+      delete[] dotf;
+    }
+
+  }
+  else cout << "Can't open dot file" << endl;
+
+  dotfile.close();
 
     for(uint32_t i=0; i < size; i++){
       if(memblock[i] == '#'){
         if(strncmp(&memblock[i], "#include",8) == 0){
 	  cout << "We find #include" << endl;
 
-	  size_t namelen = calcNameLength(&memblock[i+8]);
-	  cout << "Name length is:" << namelen << endl;
-	  char *filename = new char[256];
-	  snprintf(filename, namelen+1,"%s",(char*)&memblock[i+10]);
+	  printfilename(&memblock[i]);
 	  
-	  //ifstream file (filename, ios::in | ios::binary | ios::ate);
-	  ifstream file(filename, ios::in | ios::binary | ios::ate);
-	  cout << "File name is: "<< filename << endl;
-
-	  if (file.is_open()) cout << "File is opened" << endl;
-	  else{
-	    char *p1name = new char[1024];
-	    snprintf(p1name, 1024, "/user/include/c++/5.3.1/%s",filename);
-	    ifstream file (filename, ios::in | ios::binary | ios::ate);
-	    if (file.is_open()) cout << "File is opened" << endl;
-	    else{
-	      snprintf(p1name, 1024, "/user/local/include/%s",filename);
-	      ifstream file (filename, ios::in | ios::binary | ios::ate);
-	      if (file.is_open()) cout << "File is opened" << endl;
-	    } 
-	    cout << "Unable to open file" << endl;
-	    exit(1);
-	  }
-	  
-	  streampos length = file.tellg();
-	  char *filecontent = new char[length+1];
-
-	  file.seekg (0, ios::beg);
-	  file.read(filecontent, length);
-	  file.close();		 
-	  sometree.addLeaf(argv[1], filename,(uint8_t*)filecontent, length );
 	}
 	
-	if(strncmp(&memblock[i], "#define",7) == 0) cout << "We find #include" << endl;
+	if(strncmp(&memblock[i], "#define",7) == 0) cout << "We find #define" << endl;
 
       }
     }
